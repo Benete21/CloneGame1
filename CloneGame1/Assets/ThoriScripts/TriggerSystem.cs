ad4283d7cc93c2a441acaa4a4384cfea4ba7c72d
therealthori <2570507@students.wits.ac.za>
2025-07-31 11:46:14 +0200

    Camera Follow and Start of Trigger Action.

---
+++ CloneGame1/Assets/ThoriScripts/TriggerSystem.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSystem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
