10e5fde9ac1dcd8864661b0a7878a5d50269be13
NtsikeleloMadlongolwane <2154536@students.wits.ac.za>
2025-08-01 22:44:32 +0200

    Animations done. Still need to make animations blend tree

--- CloneGame1/Assets/ThoriScripts/PlayerMovement.cs
+++ CloneGame1/Assets/ThoriScripts/PlayerMovement.cs
@@ -8,24 +8,27 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5;
     public Rigidbody2D rb;
     private Vector2 movement;
-    //public Animator animator;
+    
+    private Animator animator;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         
-        /*animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);*/
-        
-        
+       //animator.SetFloat("Horizontal", movement.x);
+       // animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        
+        animator.SetFloat("xVelocity", movement.x);
     }
 }
