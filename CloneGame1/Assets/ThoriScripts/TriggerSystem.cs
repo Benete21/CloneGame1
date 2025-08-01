d853a2dc8308d1f220a8def02c5c470877f1cb31
therealthori <2570507@students.wits.ac.za>
2025-08-01 15:56:51 +0200

    Camera Trigger Works
    Did the right things

--- CloneGame1/Assets/ThoriScripts/TriggerSystem.cs
+++ CloneGame1/Assets/ThoriScripts/TriggerSystem.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 
 public class TriggerSystem : MonoBehaviour
 {
+    public CameraFollow cameraFollow;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        throw new NotImplementedException();
+        /*if (other.gameObject.name == "EnemyFight")
+        {
+            Debug.Log("Test");
+        }*/
+        
+        if (other.CompareTag("Enemy"))
+        {
+            cameraFollow.EnterBattle(transform);
+        }
+    }
+    
+    
+
+// When battle ends
+    void EndBattle()
+    {
+        cameraFollow.ExitBattle();
     }
 
     // Update is called once per frame
