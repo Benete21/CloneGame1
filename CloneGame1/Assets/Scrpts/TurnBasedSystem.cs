9d45c24ab34e58b22cb7f37a45308d9f3706ee5d
benete21 <benyubun@gmail.com>
2025-08-01 09:24:46 +0200

    Adding Attack Numbers

--- CloneGame1/Assets/Scrpts/TurnBasedSystem.cs
+++ CloneGame1/Assets/Scrpts/TurnBasedSystem.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TurnBasedSystem : MonoBehaviour
 {
@@ -10,25 +12,52 @@ public class TurnBasedSystem : MonoBehaviour
     public int Enemy_Health_Max;
     public int Enemy_Health_Curr;
 
-    public int [] attack = new int [4];
+    public int damage;
     public int Default;
     public int Brave;
     public int Gold;
     public int Player_Speed;
     public int Enemy_Speed;
+    public int BP_Points;
+
 
     public bool PlayerTurn;
     public bool EnemyTurn;
 
+    public GameObject AttackUI;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void PlayerAttack()
+    {
+        AttackUI.SetActive (true);
+    }
+    public void SelectAttackCard()
+    {
+        
+    }
+    public void Attack_01()
+    {
+        damage += Random.Range(1,2);    
+    }
+    public void Attack_02()
+    {
+        damage += Random.Range(1, 2);
+    }
+    public void Attack_03()
+    {
+        damage += Random.Range(1, 2);
+    }
+    public void Attack_04()
+    {
+        damage += Random.Range(1, 2);
     }
 }
