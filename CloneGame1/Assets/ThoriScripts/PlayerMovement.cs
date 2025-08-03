7a683217ccd46c45af861e56999a56f49531f5c3
Truth2004 <nhlanzekomwandla@gmail.com>
2025-08-03 19:53:26 +0200

    Updated
    Added animations, fixed movement

--- CloneGame1/Assets/ThoriScripts/PlayerMovement.cs
+++ CloneGame1/Assets/ThoriScripts/PlayerMovement.cs
@@ -1,31 +1,37 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5;
+    public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    private Vector2 movement;
-    //public Animator animator;
+    public Transform characterVisual;
 
-    // Update is called once per frame
+    private Vector2 movement;
+    private Vector2 currentVelocity;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        
-        /*animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);*/
-        
-        
+        Vector2 rawInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        movement = Vector2.SmoothDamp(movement, rawInput, ref currentVelocity, 0.05f);
+
+        if (movement.x > 0)
+            characterVisual.localScale = new Vector3(1, 1, 1);
+        else if (movement.x < 0)
+            characterVisual.localScale = new Vector3(-1, 1, 1);
+
+        anim.SetFloat("speed", movement.magnitude);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        
     }
 }
